5194ab622ee31851301390321affdb7e76621042
Jon.King <jinpujun@gmail.com>
2014-09-10 00:48:47 +0800

    dms test 100%

--- sql/patch.sql
+++ sql/patch.sql
@@ -2,7 +2,8 @@
 UPDATE um_application t SET t.`applicationId` = 'dms' WHERE t.`applicationId`='dm';
 UPDATE um_resourcetype t SET t.`applicationId` = 'dms' WHERE t.`applicationId`='dm';
 UPDATE um_resourcetype_root t SET t.`applicationId` = 'dms' WHERE t.`applicationId`='dm';
+UPDATE um_operation t SET t.`applicationId` = 'dms' WHERE t.`applicationId`='dm';
 
 
 UPDATE dm_report t SET t.param = REPLACE( t.param, '../display/json/', '../../display/json/');
-SELECT t.`param` FROM dm_report t WHERE t.param  LIKE '%jsonUrl%';
+-- SELECT t.`param` FROM dm_report t WHERE t.param  LIKE '%jsonUrl%';
