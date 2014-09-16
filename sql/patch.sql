13c9ddcd55f5c0123b51388a0b24669bcf0d4db7
waitwind <jinpujun@gmail.com>
2014-09-16 18:20:31 +0800

    sync from wmsbi

--- sql/patch.sql
+++ sql/patch.sql
@@ -1,4 +1,4 @@
--- 变更applicationId为dms
+﻿-- 变更applicationId为dms
 UPDATE um_application t SET t.`applicationId` = 'dms' WHERE t.`applicationId`='dm';
 UPDATE um_resourcetype t SET t.`applicationId` = 'dms' WHERE t.`applicationId`='dm';
 UPDATE um_resourcetype_root t SET t.`applicationId` = 'dms' WHERE t.`applicationId`='dm';
@@ -14,3 +14,10 @@ UPDATE dm_report t SET t.param = REPLACE( t.param, '../service/', '../../service
 
 UPDATE dm_report t SET t.displayUri = REPLACE( t.displayUri, '../common/ichart.html', 'ichart.html');
 UPDATE dm_report t SET t.displayUri = REPLACE( t.displayUri, '../wms/kanban', '../../wms/kanban');
+
+
+打包注意：
+1、更改portal/model下Logo图标（BTR 、WMS）
+2、tssUtil.js里 SYSTEM_TITLE 更改
+3、#class.name.IdentityGetter = com.jinhe.tss.um.sso.UMIdentityGetter
+    class.name.IdentityGetter = com.jinhe.tss.um.sso.othersystem.WmsIdentifyGetter
