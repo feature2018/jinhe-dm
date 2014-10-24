d548e78892f905905f2efea96df6b81c4b5be620
waitwind <jinpujun@gmail.com>
2014-10-24 12:56:09 +0800

    btrbi deploy to 72.80

--- src/main/webapp/tools/tssJS/tssJS.all.js
+++ src/main/webapp/tools/tssJS/tssJS.all.js
@@ -1277,8 +1277,8 @@
     _HTTP_RESPONSE_STATUS_LOCAL_OK  = 0,    // 本地OK
     _HTTP_RESPONSE_STATUS_REMOTE_OK = 200,  // 远程OK
 
-    /* HTTP超时(1分钟) */
-    _HTTP_TIMEOUT = 1*60*1000,
+    /* HTTP超时(3分钟) */
+    _HTTP_TIMEOUT = 3*60*1000,
 
  
     /*
